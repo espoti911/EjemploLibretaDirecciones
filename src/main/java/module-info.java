2ffcac64091b4076199c69dd3e2bfc8806d7f4f0
espoti911 <franciscomanuelgonzalezmartin@gmail.com>
2024-11-19 18:03:32 +0100

    Ejemplo de una libreta de direcciones con FXML

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module es.ieslosmontecillos.ejemplolibretadireccionesfxml {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens es.ieslosmontecillos.ejemplolibretadireccionesfxml to javafx.fxml;
+    exports es.ieslosmontecillos.ejemplolibretadireccionesfxml;
+}
